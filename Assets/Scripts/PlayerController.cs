11c62b85250114ffc156a2345a11de485f5e208b
sholev <asen.sholev@gmail.com>
2016-03-12 23:41:23 +0200

    New player spaceship and weapon type.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class Boundary
 {
-    public float xMin, xMax, yMin, yMax;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
 }
 
 public class PlayerController : MonoBehaviour
@@ -13,39 +14,40 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float tilt;
     public Boundary boundary;
-    public ParticleSystem[] engineParticles;
 
-    public GameObject shot;
-    public Transform shotSpawn;
+    public GameObject projectile;
+    public Transform projectileSpawn;
     public float fireRate;
 
     private float nextFire;
 
-    void Update()
+    public void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if (Input.GetButton("Fire1") && Time.time > this.nextFire)
         {
-            nextFire = Time.time + fireRate;
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-            GetComponent<AudioSource>().Play();
+            this.nextFire = Time.time + this.fireRate;
+            UnityEngine.Object.Instantiate(
+                this.projectile, this.projectileSpawn.position, this.projectileSpawn.rotation);
+            UnityEngine.Object.Instantiate(
+                this.projectile, this.projectileSpawn.position, this.projectileSpawn.rotation);
+            this.GetComponent<AudioSource>().Play();
         }
     }
 
-    void FixedUpdate()
+    public void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-        GetComponent<Rigidbody>().velocity = movement * speed;
+        this.GetComponent<Rigidbody>().velocity = movement * this.speed;
 
-        GetComponent<Rigidbody>().position = new Vector3(
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            Mathf.Clamp(GetComponent<Rigidbody>().position.y, boundary.yMin, boundary.yMax),
+        this.GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(this.GetComponent<Rigidbody>().position.x, this.boundary.xMin, this.boundary.xMax),
+            Mathf.Clamp(this.GetComponent<Rigidbody>().position.y, this.boundary.yMin, this.boundary.yMax),
             0.0f);
 
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler(GetComponent<Rigidbody>().velocity.y * -tilt, 90.0f, 0.0f);
-
-        this.engineParticles[0].startRotation = GetComponent<Rigidbody>().rotation.x + -1.5708f;
+        this.GetComponent<Rigidbody>().rotation = Quaternion.Euler(
+            this.GetComponent<Rigidbody>().velocity.y * -this.tilt, 90.0f, 0.0f);
     }
 }
