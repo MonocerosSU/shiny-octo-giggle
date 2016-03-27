e18c81e3ff228d3c3a5014dfaa047fed2ceec0b3
GSimov <georgsimov@abv.bg>
2016-03-27 18:43:31 +0300

    Added Kamikadze Enemy.

---
+++ Assets/Scripts/Charge.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charge : MonoBehaviour {
+	public Transform target;
+	public float thrust;
+	public Rigidbody rigidBody;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody>();
+		transform.LookAt(target);
+		}
+
+	void Update() {
+		rigidBody.AddForce(transform.forward * thrust);
+	}
+}
