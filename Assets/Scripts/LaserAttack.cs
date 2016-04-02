d0aa70548fb1bd3654a5430f95552df7b8747ef7
sholev <asen.sholev@gmail.com>
2016-04-02 23:20:32 +0300

    The laser weapon is damaging enemies now.

--- Assets/Scripts/LaserAttack.cs
+++ Assets/Scripts/LaserAttack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class LaserAttack : MonoBehaviour
 {
@@ -17,7 +18,9 @@ public class LaserAttack : MonoBehaviour
     public GameObject warmUpEffect;
 
     [Header("Other parameters:")]
+    public float laserDamage = 13f;
     public float maxLaserSize = 18f;
+    public GameObject damageEffect;
 
     private GameObject start;
     private GameObject middle;
@@ -25,9 +28,11 @@ public class LaserAttack : MonoBehaviour
 
     private float effectCooldown;
     private float warmUpTime;
+    private float graduallyIncreasingLaserSize;
 
     public void Start()
     {
+        this.graduallyIncreasingLaserSize = 1;
         this.effectCooldown = 0;
         this.warmUpTime = this.inputWarmUpTime;
     }
@@ -46,6 +51,7 @@ public class LaserAttack : MonoBehaviour
         }
         else
         {
+            this.graduallyIncreasingLaserSize = 1;
             this.warmUpTime = this.inputWarmUpTime;
             if (this.end != null || this.middle != null || this.start != null)
             {
@@ -62,13 +68,18 @@ public class LaserAttack : MonoBehaviour
         {
             return;
         }
-
+        
+        // This check prevents spawning too much of the effect.
         if (this.effectCooldown <= 0)
         {
             this.effectCooldown = this.inputEffectCooldown;
             var newEffect = GameObject.Instantiate(
                 this.warmUpEffect, this.laserSpawn.position, this.laserSpawn.rotation) as GameObject;
-            newEffect.transform.parent = this.transform;
+
+            if (newEffect != null)
+            {
+                newEffect.transform.parent = this.transform;
+            }
         }
 
         this.effectCooldown -= Time.deltaTime;
@@ -81,19 +92,31 @@ public class LaserAttack : MonoBehaviour
         this.InstantiateWeaponPart(ref this.middle, ref this.laserMiddle);
         this.InstantiateWeaponPart(ref this.end, ref this.laserEnd);
 
-        float currentLaserSize = this.maxLaserSize;
+        this.graduallyIncreasingLaserSize++;
+        float currentLaserSize = Mathf.Min(this.maxLaserSize, this.graduallyIncreasingLaserSize);
 
         // Raycast at the right as our sprite has been design for that
         Vector3 laserDirection = this.laserSpawn.forward;
-        RaycastHit hit;
-        bool isHittingTarget = Physics.Raycast(this.laserSpawn.position, laserDirection, out hit, this.maxLaserSize);
-        bool hitTargetIsNotParent = isHittingTarget && !this.CompareTag(hit.collider.tag);
-        if (hitTargetIsNotParent)
+        
+        RaycastHit[] hits = Physics.RaycastAll(this.laserSpawn.position, laserDirection);
+        RaycastHit hit = this.CheckHits(hits, currentLaserSize);
+        
+        if (hit.collider != null)
         {
-            //Debug.Log("Touching " + hit.collider.gameObject.name);
+            //Debug.Log("Hitting " + hit.collider.gameObject.name);
 
-            // -- Get the laser length
-            currentLaserSize = Vector3.Distance(hit.point, this.laserSpawn.position);
+            // Get the distance to target
+            currentLaserSize = Mathf.Min(
+                Vector3.Distance(hit.point, this.laserSpawn.position),
+                this.graduallyIncreasingLaserSize);
+
+            // Damage target
+            DestroyByHitpoints hitpointsClass = hit.collider.GetComponent<DestroyByHitpoints>();
+            if (hitpointsClass != null)
+            {
+                hitpointsClass.TakeDamage(this.laserDamage);
+                this.PlayDamageEffect();
+            }
         }
 
         // Place things
@@ -111,6 +134,28 @@ public class LaserAttack : MonoBehaviour
         {
             this.end.transform.localPosition = new Vector3(0f, 0f, currentLaserSize);
         }
+    }
+
+    private void PlayDamageEffect()
+    {
+        // TODO: Implement me.
+    }
+
+    private RaycastHit CheckHits(RaycastHit[] hits, float maxDistance)
+    {
+        RaycastHit resultHit = new RaycastHit();
+
+        if (hits.Length == 0)
+        {
+            return resultHit;
+        }
+
+        resultHit = hits.Where(hit => hit.collider != null)
+            .Where(hit => hit.collider.CompareTag("Enemy") || hit.collider.CompareTag("Shield"))
+            .OrderBy(hit => Vector3.Distance(hit.point, this.laserSpawn.position))
+            .FirstOrDefault();
+
+        return resultHit;
     }
 
     private void InstantiateWeaponPart(ref GameObject destination, ref GameObject source)
