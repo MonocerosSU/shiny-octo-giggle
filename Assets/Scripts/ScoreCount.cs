e68f7b0be251b0accebdbec4eb33070ddbb0f78a
GSimov <georgsimov@abv.bg>
2016-04-05 04:27:12 +0300

    Game now has Highscore

--- Assets/Scripts/ScoreCount.cs
+++ Assets/Scripts/ScoreCount.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 using UnityEngine.UI;
 
 public class ScoreCount : MonoBehaviour {
 
+	public Text highscore;
 	public Text scoreText;
-	private int score;
+	public int score;
+	public int highScore;
+	string highScoreKey = "Highscore";
+
 
 	void Start ()
 	{
 		score = 0;
 		UpdateScore ();
+		highScore = PlayerPrefs.GetInt(highScoreKey,0);    
 	}
 
 	public void AddScore (int newScoreValue)
@@ -20,8 +24,29 @@ public class ScoreCount : MonoBehaviour {
 		UpdateScore ();
 	}
 
+	void Update ()
+	{
+		highscore.text = "Highscore:" + highScore;
+		if (score > highScore) 
+		{
+			highscore.text = "Highscore: " + score ;
+		}
+	}
+
 	void UpdateScore ()
 	{
 		scoreText.text = "Score: " + score;
+		highscore.text = "Highscore: " + highScore;
 	}
+	void OnDisable(){
+
+		//If our scoree is greter than highscore, set new higscore and save.
+		if(score>highScore)
+		{
+			PlayerPrefs.SetInt(highScoreKey, score);
+			PlayerPrefs.Save();
+		}
+	}
+
+
 }
