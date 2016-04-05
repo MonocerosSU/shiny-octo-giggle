e8ff02ee1a70356c32f05760c373e18488faac80
GSimov <georgsimov@abv.bg>
2016-04-05 12:39:01 +0300

    Cursor is only visible in the Menu Scene

---
+++ Assets/Scripts/NoCursor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoCursor : MonoBehaviour {
+
+	void Start ()
+	{
+		Cursor.visible = false;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Application.LoadLevel ("MenuScene");
+			Cursor.visible = true;
+		}
+	}
+}
